c76abbc8b742c3dbeb0371da8e10b61eee4fe50a
Peter <peterdd06@gmail.com>
2026-08-31 19:27:38 -0400

    feat(api): add ticket retrieval endpoint

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const pool = require("./db")
 
 const PORT = 3000;
 
@@ -7,6 +8,23 @@ app.get("/", (request, response) => {
     response.send("RepairDesk API is working")
 });
 
+
+app.get("/api/tickets", async (request, response) => {
+    try{
+        const result = await pool.query(
+            "SELECT * FROM tickets ORDER BY created_at DESC"
+        );
+        response.status(200).json(result.rows);
+        console.log("get requests followed through ")
+        
+    }
+    catch (error) {
+        console.error("Error fetching tickets", error);
+        response.status(500).json({
+            error: "Failed to fetch tickets"
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
